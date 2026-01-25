a9efac27d0cd20dac89efe342710fd3e423c8c82
ikailash19 <kailashiviswanathan@gmail.com>
2026-01-25 17:01:35 +0530

    Day 2 - Conditions and boolean logic

---
+++ basics/day2_conditions.py
@@ -0,0 +1,19 @@
+a = True
+b = False
+
+print(type(a)) # <class 'bool'>
+print(10 > 5) # True
+print(10 == "10") # False
+
+age = 18
+
+if age >= 18:
+    print("Adult")
+else:
+    print("Minor")
+
+x = 10
+
+print(x > 5 and x < 20) # True
+print(x < 5 or x == 10) # True
+print(not(x == 10)) # False
