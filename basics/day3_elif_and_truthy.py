f15a9b67677f3666efd61ce5192377a989b93f80
ikailash19 <kailashiviswanathan@gmail.com>
2026-01-26 16:34:11 +0530

    Day 3 - elif, nested conditions, truthy and falsy

---
+++ basics/day3_elif_and_truthy.py
@@ -0,0 +1,29 @@
+score = 59
+
+if score >= 90:
+    print("A")
+elif score >= 75:
+    print("B")
+elif score >= 60:
+    print("C")
+else:
+    print("Fail")
+
+age = 25
+has_id = True
+
+if age >= 18:
+    if has_id:
+        print("Allowed")
+    else:
+        print("ID required")
+else:
+    print("Too young")
+
+values = [0, 1, "", "text", [], [1,2], None]
+
+for v in values:
+    if v:
+        print(v, "-> Truthy")
+    else:
+        print(v, "-> Falsy")
